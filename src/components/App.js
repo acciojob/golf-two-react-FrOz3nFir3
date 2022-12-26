deb20dfd49cea4a4b336578e9d7a62910ed8d791
Kush Surti <30488380+FrOz3nFir3@users.noreply.github.com>
2022-12-26 17:02:54 +0530

    using callback and updating state directly

--- src/components/App.js
+++ src/components/App.js
@@ -8,24 +8,43 @@ const App = () => {
     left: "0px",
     top: "0px",
   });
-  const handleBall = (event) => {
+  const handleBall = React.useCallback((event) => {
     if(event.keyCode == 39){
       // Right arrow
-       setX((x)=>x+5)
+      setBallPosition((prevState) => {
+        return {
+          ...prevState,
+          left: `${parseInt(prevState.left)+5}px`
+        }
+      })
     }else if(event.keyCode == 37){
       // left Arrow
-      setX((x)=>x-5)
-
+      setBallPosition((prevState) => {
+        return {
+          ...prevState,
+          left: `${parseInt(prevState.left)-5}px`
+        }
+      })
     }else if(event.keyCode == 38){
       // up arrow
-      setY((y)=>y-5)
-
+      // setY((y)=>y-5)
+      setBallPosition((prevState) => {
+        return {
+          ...prevState,
+          top: `${parseInt(prevState.top) - 5}px`
+        }
+      })
     }else if(event.keyCode == 40){
       // down arrow
-      setY((y)=>y+5)
-
+      // setY((y)=>y+5)
+      setBallPosition((prevState) => {
+        return {
+          ...prevState,
+          top: `${parseInt(prevState.top)+5}px`
+        }
+      })
     }
-  };
+  },[]);
 
   React.useEffect(() => {
     window.addEventListener("keydown", handleBall, false);
@@ -45,7 +64,7 @@ const App = () => {
   return (
     <div className="playground">
       {renderBall ? (
-        <div style={{top:`${y}px`, left:`${x}px`}} className="ball"></div>
+        <div style={ballPosition} className="ball"></div>
       ) : (
         <button onClick={play} className="play">
           Play
